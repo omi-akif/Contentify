091a580eb09bbcb43bf14326bd2b50edfe189ee9
omi-akif <omiakif@gmail.com>
2020-11-13 22:02:06 +0600

    edited config file

---
+++ config.php
@@ -0,0 +1,23 @@
+<?php 
+    ini_set("display_errors", true);
+    date_default_timezone_get("Asia/Dhaka");
+
+    define("DB_DSN", "mysql:host=localhost;dbname=content");
+
+    define("DB_USERNAME", "root");
+    define("DB_PASSWORD", "");
+    define("CLASS_PATH", "classes");
+    define("TEMPLATE_PATH", "templates");
+    define("HOMEPAGE_NUM_ARTICLES", 5);
+    define("ADMIN_USERNAME", "root");
+    define("ADMIN_PASSWORD", "root");
+    
+    require(CLASS_PATH  . "/Article.php");
+
+    function handleException($exception){
+        echo "Sorry, a problem occured. Please try later.";
+        error_log($exception->getMessage());
+    }
+
+    set_exception_handler('handleException');
+?>
