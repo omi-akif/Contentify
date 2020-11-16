2e241b07e9c1ee0f527a1778a57f07c1a68af8a9
omi-akif <omiakif@gmail.com>
2020-11-16 22:25:23 +0600

    added Login Form

---
+++ templates/admin/loginForm.php
@@ -0,0 +1,37 @@
+<?php include "templates/include/header.php"?>
+
+    <form action="admin.php?action=login" method="post" style="width: 50%">
+
+    <input type="hidden" name="login" value="true">
+
+<?php if (isset($results['errorMessage'])){ ?> 
+    
+    <div class="errorMessage">
+    
+        <?php echo $results['errorMessage'] ?>
+    
+    </div>
+
+<?php } ?>
+
+    <ul>
+        <li>
+            <label for="username ">Username</label>
+            <input type="text" name="username" id="username" placeholder="Enter your username" required autofocus maxlenght="20">
+        </li>
+
+        <li>
+            <label for="password">Password</label>
+            <input type="password" name="password" id="password" placeholder="Enter your password" required maxlenght="20">
+        </li>
+
+        
+    </ul>
+
+    <div class="buttons">
+        <input type="submit" name="login" value="Login">
+    </div>
+
+    </form>
+
+<?php include "templates/include/footer.php"?>
