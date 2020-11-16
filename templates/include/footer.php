cdae8b05b0ce65762caaf9a53ec19f77485f095a
omi-akif <omiakif@gmail.com>
2020-11-16 19:34:09 +0600

    added footer and modified header

---
+++ templates/include/footer.php
@@ -0,0 +1,8 @@
+<div id="footer">
+            My Blog; 2020. All rights reseraved. 
+            <a href="admin.php">Admin</a>
+        </div>
+
+        </div>
+    </body>
+</html>
--- templates/include/header.php
+++ templates/include/header.php
@@ -8,11 +8,3 @@
     <body>
         <div id="container">
             <a href="."><img src="images/logo.jpg" alt="Logo"></a>
-        </div>
-
-        <div id="footer">
-            My Blog; 2020. All rights reseraved. 
-            <a href="admin.php">Admin</a>
-        </div>
-    </body>
-</html>
