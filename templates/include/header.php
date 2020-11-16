133180d079bc007d9e6c8bf24c9619b9f863a0f9
omi-akif <omiakif@gmail.com>
2020-11-16 15:00:44 +0600

    added header

---
+++ templates/include/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title><?php echo htmlspecialchars($results['pageTitle'])?></title>
+        <link rel="stylesheet" href="style.css" type="text/css">
+    </head>
+
+    <body>
+        <div id="container">
+            <a href="."><img src="images/logo.jpg" alt="Logo"></a>
+        </div>
+
+        <div id="footer">
+            My Blog; 2020. All rights reseraved. 
+            <a href="admin.php">Admin</a>
+        </div>
+    </body>
+</html>
