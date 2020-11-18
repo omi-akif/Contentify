d83f525acb33c7539e50acc2a5052634959c22ce
omi-akif <omiakif@gmail.com>
2020-11-18 11:51:30 +0600

    modified editArticle.php

--- templates/admin/editArticle.php
+++ templates/admin/editArticle.php
@@ -21,4 +21,57 @@
         <?php echo $results['pageTitle'] ?>
     </h1>
 
-    <form action="admin.php?action=<?php echo $results['articles']->ID ?>"></form>
+    <form action="admin.php?action=<?php echo $results['articles']->ID ?>">
+
+<?php if(isset($results['errorMessage'])) { ?>
+
+    <div class="errorMessage">
+        <?php echo $results['errorMessage'] ?>
+    </div>
+
+<?php } ?>
+
+    <ul>
+        <li>
+           <label for="article_title">Article Title</label> 
+           <input type="text" name="Title" id="article_title" placeholder="Name of the article" required autofocus maxlength="255" value="<?php echo htmlspecialchars($results['article']->article_title)?>">
+        </li>
+
+        <li>
+            <label for="article_summary">Article Summary</label>
+            <textarea name="Summary" id="article_summary" placeholder="Brief Description" required maxlength="1000" style="height: 5em;">
+            
+            <?php echo htmlspecialchars($results['article']->article_summary)?>
+            </textarea>
+        </li>
+
+        <li>
+            <label for="article_content">Article Content</label>
+            <textarea name="Content" id="article_content" placeholder="Your Content" required maxlength="100000" style="height: 30em;">
+
+            <?php echo htmlspecialchars($results['article']->article_content)?>
+
+            </textarea>
+        </li>
+
+        <li>
+            <label for="date_of_publication">Date</label>
+
+            <input type="date" name="Date" id="date_of_publication" placeholder="YYYY-MM-DD" required maxlength="10" value="<?php echo $results['article']->publicationDate ? date( "Y-m-d", $results['article']->publicationDate ) : "" ?>">
+        </li>
+    </ul>
+
+    <div class="buttons">
+        <input type="submit" name="saveChanges" value="Save Changes">
+        <input type="submit" formnovaludate name="cancel" value="Cancel">
+    </div>
+
+    </form>
+
+<?php if ($results['article']->ID) { ?>
+    <p>
+    <a href="admin.php?action=deleteArticle&amp;articleId=<?php echo $results['article']->ID ?>" onclick="return confirm('Delete This Article?')">Delete This Article</a>
+    </p>
+<?php } ?>
+
+<?php include "templates/include/footer.php" ?>
